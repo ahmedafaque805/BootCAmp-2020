3694a70437318768a885f779d36fe865e3d8e052
Afaque <you@example.com>
2020-07-03 13:59:25 +0500

    Asyn Javascript

---
+++ Asyn javascript/app.js
@@ -0,0 +1,89 @@
+// console.log("start group"); // 0.0005
+
+// let data = "content" //  0.0005
+
+// setTimeout(function(){
+//     console.log(data) // API // 1 second
+// }, 1000)
+
+// console.log("end group") // 0.0005
+
+
+
+
+// console.log("start group")
+
+// function breakfast(){
+// setTimeout( function(){
+// console.log("Breakfast complete")
+// },100)
+// }
+
+
+// function morning(){
+//     console.log("breakfast start")
+//     breakfast()
+// }
+// morning()
+
+// console.log("End group")
+
+
+  
+
+// ***************************** Call Back ********************************************
+
+
+// function printgreeting(msg, alertGreet){
+//     if (msg === 'evening'){
+//         evening(alertGreet)
+//     }else if (msg === 'morning'){
+//         morning(alertGreet)
+//     }else{
+//         night(alertGreet);
+//     }
+// }
+
+// function morning(){
+//     alert("Good Morning")
+//     alertGreet("hello world")
+// }
+
+// function evening(){
+//     alert("Good evening")
+//     alertGreet("hello world")
+// }
+
+// function night(){
+//     alert("Good Night")
+//     alertGreet("hello world")
+// }
+
+// function alertGreet(){
+//     alert("hello world")
+// }
+
+// printgreeting("hello world",alertGreet)
+
+
+
+
+// **************************  Promises **************************************
+
+
+
+// let promise = new Promise( function(resolve, reject){
+
+//     setTimeout(function(){
+//         console.log("new Promise");
+//         resolve()    
+//     }, 1000)
+    
+// })
+// promise.then( function(){
+//     console.log("promise resolve");  
+// })
+
+
+
+
